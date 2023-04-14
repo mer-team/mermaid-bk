f3e1730b4dbece84b2fc293e633ec8d70c7a6d8c
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-04-14 16:25:44 +0100

    Novas rotas

---
+++ src/controllers/SongController.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const {Song} = require('../models/index')
+
+module.exports = {
+
+    //Get the Songs in the database
+    async index(req, res){
+        try{
+           const songs = await Song.findAll()
+           return res.status(200).json(songs)
+        }catch(e){
+            console.log(e)
+        }
+    }, 
+
+}
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,22 @@ const express = require('express')
 const route = express.Router()
 
 const UserController = require('./controllers/UserController')
-const { validateToken } = require('./JWT')
+const SongController = require('./controllers/SongController')
+const SongClassificationController = require('./controllers/SongClassificationController')
 
+const { validateToken } = require('./JWT')
 //Register User on the database  
 route.post("/signup", UserController.store)
 //Login the user with JWT
 route.post("/login", UserController.index)
 //Get user data by the JWT
 route.get("/user", validateToken, UserController.show)
+
+//Get all the songs in the database
+route.get("/song", SongController.index)
+
+//Get the song classification given the id of the song 
+route.get("/classifications", SongClassificationController.index)
 
 
 module.exports = route
