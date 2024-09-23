ecc421b9b77853f1d9bc7e986158be309f576e67
tiag021 <aluno22102@ipt.pt>
2024-09-23 14:42:59 +0100

    Song Processing routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -5,12 +5,14 @@ const userRoutes = require('./UserRoutes');
 const songRoutes = require('./SongRoutes');
 const songClassificationRoutes = require('./SongClassificationRoutes');
 const feedbackRoutes = require('./FeedbackRoutes');
+const songProcessingRoutes = require("./SongProcessingRoutes");
 const { sendMessage } = require('../Services/rabbitmqService');
 
 // Aggregate routes
 router.use('/user', userRoutes);
 router.use('/song', songRoutes);
 router.use('/classification', songClassificationRoutes);
+router.use('/processing', songProcessingRoutes);
 router.use('/feedback', feedbackRoutes);
 
 // RabbitMQ route
--- src/routes/SongProcessingRoutes.js
+++ src/routes/SongProcessingRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
 const SongProcessingController = require('../controllers/SongProcessingController');
 
 // Endpoint to receive information when a song finishes processing
-router.post('/processing/completed', SongProcessingController.handleProcessingComplete);
+router.post('/completed', SongProcessingController.handleProcessingComplete);
 
 // Endpoint to receive log information about song processing
-router.post('/processing/log', SongProcessingController.handleProcessingLog);
+router.post('/log', SongProcessingController.handleProcessingLog);
 
 // Endpoint to receive detailed song segments
-router.post('/processing/segments', SongProcessingController.handleSongSegments);
+router.post('/segments', SongProcessingController.handleSongSegments);
 
 
 module.exports = router;
