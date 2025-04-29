76ce397f2dfa8eb3d09fe41f91283ce8adde2514
LC5555 <luiscosta5555@gmail.com>
2025-04-29 18:55:30 +0000

    chore(all): Updated prettier configuration #14

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   semi: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
+  endOfLine: 'auto',
+  arrowParens: 'always',
 };
