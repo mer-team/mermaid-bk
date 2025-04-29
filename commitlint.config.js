0f0744b29367cbb257ee795d8433f36fcd957c7c
LC5555 <luiscosta5555@gmail.com>
2025-04-29 18:59:32 +0000

    chore(all): Configured commitlint rules

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'scope-enum': [2, 'always', ['api', 'web', 'mer', 'all']],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'lower-case']],
+    'subject-max-length': [2, 'always', 80], // Enforce max subject length
+  },
+};
